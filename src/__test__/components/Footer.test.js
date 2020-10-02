bd1fa1e406f53828d94a312275d145b4a2e9154c
Lalo Diaz <lalo.diaz1896@gmail.com>
2020-10-02 00:24:57 -0500

    Primeras pruebas a footer

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+    const footer = mount(
+        <MemoryRouter>
+            <Footer />
+        </MemoryRouter>
+    );
+    
     test('Render del componente Footer', () => {
-        const footer = mount(<Footer />);
         expect(footer.length).toEqual(1);
     });
+
+    test('Footer tiene que tener 1 Link', () => {
+        expect(footer.find('Link')).toHaveLength(1);
+    })
+
+    test('Footer tiene que tener 5 anchors', () => {
+        expect(footer.find('a')).toHaveLength(5);
+    })
 });
