6a107e109a5258d7c3202b89bfe7ebe55b1a66b9
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-10-04 02:18:53 -0500

    Footer Responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/components/Footer.scss';
 
 const Footer = () => {
-    return(
+    return (
         <footer className="footer">
             <section className="footer__cta">
                 <h3>Miles de personas han encontrado su Roomate apropiado, ahora es tu turno</h3>
                 <Link to='select-acount'><button className="footer__cta--button">LET'S ROOMIE</button></Link>
             </section>
             <section className="footer__icons">
-                <div>
-                    <p>© 2020 Lets Rommie Technologies, Inc.</p>
-                </div>
-                <div>
+                <div className='footer__icons--social'>
                     <p>Nuestras redes sociales</p>
-                    <a href="#"><img alt="icono de FaceBook" src="https://img.icons8.com/color/48/000000/facebook.png"/></a>
-                    <a href="#"><img alt="icono de Instagram" src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
-                    <a href="#"><img alt="icono de Twitter" src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
-                    <a href="#"><img alt="icono de whatsapp" src="https://img.icons8.com/color/48/000000/whatsapp.png"/></a>
+                    <a href="#"><img alt="icono de FaceBook" src="https://img.icons8.com/color/48/000000/facebook.png" /></a>
+                    <a href="#"><img alt="icono de Instagram" src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+                    <a href="#"><img alt="icono de Twitter" src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+                    <a href="#"><img alt="icono de whatsapp" src="https://img.icons8.com/color/48/000000/whatsapp.png" /></a>
+                </div>
+                <div className='footer__icons--copy'>
+                    <p>© 2020 Lets Rommie Technologies, Inc.</p>
                 </div>
             </section>
         </footer>
