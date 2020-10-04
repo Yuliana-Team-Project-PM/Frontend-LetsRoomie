35a22254b8441a95b7b0043e72f08dae89991714
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-10-04 16:11:26 -0500

    Íconos de whatsapp y mail

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -5,15 +5,15 @@ import { AiOutlineWhatsApp, AiOutlineMail } from 'react-icons/ai'
 import '../assets/styles/components/ContactCard.scss';
 import useRoomCardinfo from '../hooks/useRoomCardinfo';
 
-const ContactCard = ({price, email, phone}) => {
-    let mail= `mailto:${email}`
-    let Numerphone=`https://api.whatsapp.com/send?phone=${phone}&text= Hola!%20Quiero%20ser%20tu%20Roomie!`
-    return(
+const ContactCard = ({ price, email, phone }) => {
+    let mail = `mailto:${email}`
+    let Numerphone = `https://api.whatsapp.com/send?phone=${phone}&text= Hola!%20Quiero%20ser%20tu%20Roomie!`
+    return (
         <div className='ContactCard'>
             <h2 className='ContactCard__title'>${price} / mensual</h2>
             <h4 className='ContactCard__subtitle'>Contáctame vía</h4>
-            <a href={Numerphone} target="_blank" className='ContactCard__button'>WhatsApp</a>
-            <a  href={mail} target="_blank" className='ContactCard__button'>Mail</a>
+            <a href={Numerphone} target="_blank" className='ContactCard__button'>WhatsApp <AiOutlineWhatsApp size={23} /> </a>
+            <a href={mail} target="_blank" className='ContactCard__button'>Mail <AiOutlineMail size={23} /> </a>
         </div>
     );
 }
